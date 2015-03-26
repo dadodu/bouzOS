5bb0e2ce21bf9a91eb9d7db1738071502994c85a
dadodu <dadodu@gmail.com>
2015-03-26 18:43:24 +0100

    add sphinx script install

---
+++ vox/install_sphinx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# sphinxbase-0.8.tar.gz
+wget -O sphinxbase-0.8.tar.gz http://sourceforge.net/projects/cmusphinx/files/sphinxbase/0.8/sphinxbase-0.8.tar.gz/download
+tar -xvzf sphinxbase-0.8.tar.gz
+cd sphinxbase-0.8
+./configure --prefix=/usr/local
+make
+sudo make install
+cd ..
+
+# pocketsphinx-0.8.tar.gz
+wget -O pocketsphinx-0.8.tar.gz http://sourceforge.net/projects/cmusphinx/files/pocketsphinx/0.8/pocketsphinx-0.8.tar.gz/download
+tar -xvzf pocketsphinx-0.8.tar.gz
+cd pocketsphinx-0.8
+./configure --prefix=/usr/local
+make
+sudo make install
+cd ..
